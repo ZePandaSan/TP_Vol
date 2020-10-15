f6f2fc68d117053d9a462794093ae77cea680db5
Wassim SAIDANE <72514191+wasshisoka44@users.noreply.github.com>
2020-10-15 23:16:33 +0200

    Add files via upload

--- src/Start.java
+++ src/Start.java
@@ -8,20 +8,20 @@ import gestion_vol.Vol;
 
 public class Start{
 	public static void main(String[] args){
-		//----------------------------------------------------------------------------
+		//----------------------------------------------------------------------------------
 		String idvol="Air France 0004";                                              
 		ZoneId paris = ZoneId.of("Europe/Paris");
-		LocalDateTime dep = LocalDateTime.of(2020, Month.OCTOBER, 18, 00, 30);
+		LocalDateTime dep = LocalDateTime.of(2020, Month.OCTOBER, 18, 23, 30);
 		ZonedDateTime depart = dep.atZone(paris);
-		LocalDateTime arr = LocalDateTime.of(2020, Month.OCTOBER, 18, 6, 00);
+		LocalDateTime arr = LocalDateTime.of(2020, Month.OCTOBER, 19, 22, 29);
 		ZonedDateTime arrive = arr.atZone(paris);
 		Vol a = new Vol(idvol, depart, arrive);
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm, dd MMM yyyy");
-		//----------------------------------------------------------------------------
+		//-----------------------------------------------------------------------------------
 		System.out.println("Flight id : " + idvol);
 		System.out.println("Time zone : " + paris);
-		System.out.println("Departure : " + format.format(dep));
-		System.out.println("Arrival : " + format.format(arr));
+		System.out.println("Departure : " + format.format(dep) + " " + depart.getZone());
+		System.out.println("Arrival : " + format.format(arr) + " " + arrive.getZone());
 		System.out.println("Duration : " + a.get_Duree());
 	}
 	
--- src/gestion_vol/Vol.java
+++ src/gestion_vol/Vol.java
@@ -1,17 +1,28 @@
 package gestion_vol;
 
+import java.time.DayOfWeek;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 
 public class Vol {
 	private String id;
 	private ZonedDateTime depart;
-	private ZonedDateTime arrive;
+	private ZonedDateTime arrivee;
 
 	public Vol(String id, ZonedDateTime dep, ZonedDateTime arr){
 		this.id=id;
 		this.depart=dep;
-		this.arrive=arr;
+		this.arrivee=arr;
+		if (this.arrivee.isBefore(this.depart)){
+			throw new IllegalArgumentException("Arrival cannot be prior to departure");
+		}
+		if (this.depart.equals(this.arrivee)){
+			throw new IllegalArgumentException("Arrival cannot be at the same time as departure");
+		}
+		if (this.depart.minus(2,ChronoUnit.DAYS).isSupported(ChronoUnit.DAYS) && this.depart.getHour()<this.arrivee.getHour()+1){
+			throw new IllegalArgumentException("Flights over 23 hours are not supported");
+		}
 	}
 
 	private void ouvrir(){
@@ -23,22 +34,30 @@ public class Vol {
 	}
 
 	public String get_Duree(){
-		int duree_heure=this.arrive.getHour()-this.depart.getHour();
-		int duree_minute=this.arrive.getMinute()-this.depart.getMinute();
+		int duree_heure=this.arrivee.getHour()-this.depart.getHour();
+		int duree_minute=this.arrivee.getMinute()-this.depart.getMinute();
 		if (duree_heure<0){
-			duree_heure=(24-this.depart.getHour())+this.arrive.getHour();
+			duree_heure=(24-this.depart.getHour())+this.arrivee.getHour();
 		}
 		if (duree_minute < 0) {
-			duree_minute = (60 - this.depart.getMinute()) + this.arrive.getMinute();
-			if(this.arrive.getDayOfMonth()==this.depart.getDayOfMonth())
+			duree_minute = (60 - this.depart.getMinute()) + this.arrivee.getMinute();
+			if(this.arrivee.getDayOfMonth()==this.depart.getDayOfMonth())
 			duree_heure--;
 		}
+		if (this.arrivee.getDayOfMonth() != this.depart.getDayOfMonth() && this.arrivee.getHour() != this.depart
+				.getHour() && this.arrivee.getMinute() != this.depart.getMinute())
+				{
+					duree_heure--;
+				}
+
+
 		return duree_heure+":"+duree_minute;
 	}
 
 	public String get_ID(){
 		return this.id;
 	}
+
 
 	
 }

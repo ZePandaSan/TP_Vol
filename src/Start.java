3320812e6a9f888fc9e0bd82a75955e1f649913b
Wassim SAIDANE <72514191+wasshisoka44@users.noreply.github.com>
2020-10-15 21:28:44 +0200

    Add files via upload

--- src/Start.java
+++ src/Start.java
@@ -1,7 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import gestion_vol.Vol;
 
 public class Start{
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		//----------------------------------------------------------------------------
+		String idvol="Air France 0004";                                              
+		ZoneId paris = ZoneId.of("Europe/Paris");
+		LocalDateTime dep = LocalDateTime.of(2020, Month.OCTOBER, 18, 00, 30);
+		ZonedDateTime depart = dep.atZone(paris);
+		LocalDateTime arr = LocalDateTime.of(2020, Month.OCTOBER, 18, 6, 00);
+		ZonedDateTime arrive = arr.atZone(paris);
+		Vol a = new Vol(idvol, depart, arrive);
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm, dd MMM yyyy");
+		//----------------------------------------------------------------------------
+		System.out.println("Flight id : " + idvol);
+		System.out.println("Time zone : " + paris);
+		System.out.println("Departure : " + format.format(dep));
+		System.out.println("Arrival : " + format.format(arr));
+		System.out.println("Duration : " + a.get_Duree());
 	}
 	
 }
--- src/gestion_vol/Vol.java
+++ src/gestion_vol/Vol.java
@@ -1,19 +1,17 @@
 package gestion_vol;
 
-import java.sql.Date;
+import java.time.ZonedDateTime;
 
 
 public class Vol {
-	private int numero;
-	private Date depart;
-	private Date arrive;
-	private long duree;
+	private String id;
+	private ZonedDateTime depart;
+	private ZonedDateTime arrive;
 
-	public Vol(int n, Date dep, Date arr){
-		this.numero=n;
+	public Vol(String id, ZonedDateTime dep, ZonedDateTime arr){
+		this.id=id;
 		this.depart=dep;
 		this.arrive=arr;
-		this.duree=arr.getTime()-dep.getTime();
 	}
 
 	private void ouvrir(){
@@ -22,6 +20,24 @@ public class Vol {
 
 	private void fermer(){
 
+	}
+
+	public String get_Duree(){
+		int duree_heure=this.arrive.getHour()-this.depart.getHour();
+		int duree_minute=this.arrive.getMinute()-this.depart.getMinute();
+		if (duree_heure<0){
+			duree_heure=(24-this.depart.getHour())+this.arrive.getHour();
+		}
+		if (duree_minute < 0) {
+			duree_minute = (60 - this.depart.getMinute()) + this.arrive.getMinute();
+			if(this.arrive.getDayOfMonth()==this.depart.getDayOfMonth())
+			duree_heure--;
+		}
+		return duree_heure+":"+duree_minute;
+	}
+
+	public String get_ID(){
+		return this.id;
 	}
 
 	
